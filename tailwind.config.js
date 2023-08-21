202ad1a33e29486ce25d40095c9a719784589737
Malo <42148894+Jivadio@users.noreply.github.com>
2023-08-21 09:24:08 +0200

    Add searchbar + Data

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}", "./*.html"],
+  theme: {
+    fontFamily: {
+      'anton': ['Anton', 'sans-serif'],
+      'manrope': ['Manrope', 'sans-serif'],
+    },
+    extend: {},
+  },
+  plugins: [],
+}
+
