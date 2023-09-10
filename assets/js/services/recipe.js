2b70740efd40d4676c1004bbc467592f288f6e2b
Malo <42148894+Jivadio@users.noreply.github.com>
2023-09-10 13:02:24 +0200

    bug fix

--- assets/js/services/recipe.js
+++ assets/js/services/recipe.js
@@ -3,21 +3,22 @@ import { optionDropdown, dropdownSearch } from "./dropdown.js";
 import { displayCard } from "../index.js";
 import { manageTags } from "./tag.js";
 
-let filteredRecipes = []
-let filterIngredients = []
-let filterAppliance = []
-let filterUstensils = []
+let filteredRecipes = [];
+let filterIngredients = [];
+let filterAppliance = [];
+let filterUstensils = [];
 let selectedTag = [];
-
-const numberRecipes = document.getElementById("number-recipe");
 
 function findAll() {
     return recipes;
 }
 
+const numberRecipes = document.getElementById("number-recipe");
+const containerCard = document.getElementsByClassName("cardFactory");
+const noRecipeVar = document.getElementById("no-recipe");
+
 function search() {
     const searchBar = document.getElementById("searchbar");
-
     searchBar.addEventListener("keyup", (e) => {
         const searchString = e.target.value.toLowerCase();
         let baseRecipes = selectedTag.length > 0 ? filteredRecipes : findAll();
@@ -68,6 +69,14 @@ function search() {
         dropdownSearch(filterAppliance, 'appliance');
         dropdownSearch(filterUstensils, 'ustensils');
 
+        if (filteredRecipes.length == 0) {
+            noRecipe(searchString);
+        }
+        else {
+            containerCard[0].style.display = "grid";
+            noRecipeVar.style.display = "none";
+        }
+
         searchByTag(selectedTag);
 
         numberRecipes.textContent = filteredRecipes.length + " recettes";
@@ -103,6 +112,13 @@ function searchByTag(tags) {
     numberRecipes.textContent = tagRecipe.length + " recettes";
 
     return tagRecipe;
+}
+
+function noRecipe(value) {
+    console.log(value);
+    noRecipeVar.textContent = "Aucune recette ne contient '" + value + "', vous pouvez chercher « tarte aux pommes », « poisson », etc...";
+    containerCard[0].style.display = "none";
+    noRecipeVar.style.display = "block";
 }
 
 
